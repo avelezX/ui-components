ba17376e1c9c7316e1704e9e58d637cac67c6c6d
svelezsaffon <svelezsaffon@santiagacstudio.lan>
2024-03-01 18:20:42 -0600

    Test compoenent

---
+++ src/components/test/test.tsx
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+function DeleteThisButton() {
+  return (
+    <>
+      <Button href="#">Link</Button> <Button type="submit">Button</Button>
+    </>
+  );
+}
+
+export default DeleteThisButton;
