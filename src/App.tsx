7dbe4586134cf75c6361cc91e40a602f217a71db
pipex <falvarez@xerenity.co>
2024-03-05 16:44:11 -0500

    Base Button component theming per testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import '../sass/app.scss';
+import Button from './components/Button';
 
 const App = () => (
   <div>
     <h1>Xerenity UI Component Library</h1>
+    <Button>My Cool Button</Button>
   </div>
 );
 
